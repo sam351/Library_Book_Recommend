b6382bece54932c556d6adb3dd06d52c8fbb2593
sam <ksmo9191@hanmail.net>
2019-12-20 09:15:42 +0900

    Modify 191215_visualization_lowLoans.R

--- 191203_Seoul_libraries/191215_visualization_lowLoans.R
+++ 191203_Seoul_libraries/191215_visualization_lowLoans.R
@@ -1,5 +1,9 @@
+setwd("C:/Users/student/Desktop/sam/팀 프로젝트 (메인)")
+library(dplyr)
+library(ggplot2)
+
 # 1. 성북구 도서관 시각화
-load('//70.12.116.50/교육자료/R자료/6조/2018년 성북구 도서관별 대출목록.RData')
+load('2018년 성북구 도서관별 대출목록.RData')
 
 str(sb_lib_list$달빛마루도서관)
 # temp_list = sb_lib_list$달빛마루도서관
@@ -24,15 +28,20 @@ names = names(sb_lib_list)[ratio_vec > 20]
 # names = substr( names, 1, 5 ); length(unique(names)) == length(names)
 big_ratio_vec; names
 
-barplot(big_ratio_vec ~ names,
-        main = "성북구 도서관별 버려진 도서 비율 (20% 이상)",
-        xlab = "도서관", ylab = "버려진 도서 비율")
 
+plot_df = data.frame(lib = names, ratio=big_ratio_vec) %>% 
+  arrange(desc(ratio))
+ggplot(data = plot_df, aes(x=reorder(lib, -ratio), y=ratio)) +
+  geom_col() +
+  labs(title="성북구 도서관별 누적 대출건수 2건 이하 도서 비율 (20% 이상)",
+       y = '대출건수 2건 이하 도서 비율(%)',
+       x = '도서관') +
+  ylim(0, 100)
 
 
 
 # 2. 도봉구 도서관 시각화
-load('//70.12.116.50/교육자료/R자료/6조/2018년 도봉구 도서관별 대출목록.RData')
+load('2018년 도봉구 도서관별 대출목록.RData')
 
 ratio_vec = c()
 for (lib in names(gu_lib_list)) {
@@ -50,15 +59,18 @@ names = names(gu_lib_list)[ratio_vec > 45]
 # names = substr( names, 1, 7 ); length(unique(names)) == length(names)
 big_ratio_vec; names
 
-barplot(big_ratio_vec ~ names,
-        main = "도봉구 도서관별 버려진 도서 비율 (45% 이상)",
-        xlab = "도서관", ylab = "버려진 도서 비율")
-
+plot_df = data.frame(lib = names, ratio=big_ratio_vec) %>% 
+  arrange(desc(ratio))
+ggplot(data = plot_df, aes(x=reorder(lib, -ratio), y=ratio)) +
+  geom_col() +
+  labs(title="도봉구 도서관별 버려진 도서 비율 (45% 이상)",
+       y = '대출건수 2건 이하 도서 비율(%)',
+       x = '도서관')
 
 
 
 # 3. 강북구 도서관 시각화
-load('//70.12.116.50/교육자료/R자료/6조/2018년 강북구 도서관별 대출목록.RData')
+load('2018년 강북구 도서관별 대출목록.RData')
 
 ratio_vec = c()
 for (lib in names(gu_lib_list)) {
@@ -76,6 +88,11 @@ names = names(gu_lib_list)[ratio_vec > 20]
 # names = substr( names(gu_lib_list), 1, 6 ); length(unique(names)) == length(names)
 # big_ratio_vec; names
 
-barplot(big_ratio_vec ~ names,
-        main = "강북구 도서관별 버려진 도서 비율 (20% 이상)",
-        xlab = "도서관", ylab = "버려진 도서 비율")
+plot_df = data.frame(lib = names, ratio=big_ratio_vec) %>% 
+  arrange(desc(ratio))
+ggplot(data = plot_df, aes(x=reorder(lib, -ratio), y=ratio)) +
+  geom_col() +
+  labs(title="강북구 도서관별 버려진 도서 비율 (20% 이상)",
+       y = '대출건수 2건 이하 도서 비율(%)',
+       x = '도서관') +
+  ylim(0, 100)
